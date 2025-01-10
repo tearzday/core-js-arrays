3ccd057039a8b03cd87856dc117191d082bd1e31
Daniil Biver <d.biver@sfbtech.org>
2025-01-10 15:17:16 +0300

    feat: add shiftArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -542,8 +542,16 @@ function propagateItemsByPositionIndex(/* arr */) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  let arrResult = [];
+
+  if (n < 0) {
+    arrResult = arr.splice(0, -n);
+    return arr.concat(arrResult);
+  }
+
+  arrResult = arr.splice(-n);
+  return arrResult.concat(arr);
 }
 
 /**
