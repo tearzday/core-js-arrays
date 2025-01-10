a40f7dbaffecb5b01d5c8f82afdac3a3b4b919f5
Daniil Biver <d.biver@sfbtech.org>
2025-01-10 15:28:06 +0300

    feat: add sortDigitNamesByNumericOrder

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -567,8 +567,21 @@ function shiftArray(arr, n) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const numbersLib = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+
+  return arr.sort((a, b) => numbersLib.indexOf(a) - numbersLib.indexOf(b));
 }
 
 /**
