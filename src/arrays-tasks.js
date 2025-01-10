2bfdf9034d0a7219956c9be89f4553f75e8c4d88
Daniil Biver <d.biver@sfbtech.org>
2025-01-10 16:04:26 +0300

    feat: add findLongestIncreasingSubsequence

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -507,8 +507,22 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => longest is [3, 10] and [1, 20] => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => longest is [7, 40, 80] => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  let count = 1;
+  const sequenceCount = [];
+  nums.reduce((acc, item) => {
+    if (acc < item) {
+      count += 1;
+    } else {
+      sequenceCount.push(count);
+      count = 1;
+    }
+
+    return item;
+  });
+  sequenceCount.push(count);
+
+  return Math.max(...sequenceCount);
 }
 
 /**
