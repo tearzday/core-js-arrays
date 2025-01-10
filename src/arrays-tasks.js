a98e9ae5a527978deffb4ef692ec8b465cd1a726
Daniil Biver <d.biver@sfbtech.org>
2025-01-10 12:00:17 +0300

    feat: add sumArrays

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,16 @@ function getIntervalArray(/* start, end */) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const arrLong = arr1.length >= arr2.length ? arr1 : arr2;
+  const arrShort = arrLong !== arr1 ? arr1 : arr2;
+
+  return arrLong.map((item, index) => {
+    if (arrShort[index]) {
+      return item + arrShort[index];
+    }
+    return item;
+  });
 }
 
 /**
